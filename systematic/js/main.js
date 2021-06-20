c97ede3425750d7f817776bb79a1aac44efd4f1c
MasfiqhasanRupo <84632975+MasfiqhasanRupo@users.noreply.github.com>
2021-06-20 21:41:56 +0600

    Add files via upload

---
+++ systematic/js/main.js
@@ -0,0 +1,12 @@
+// Bx Slider
+
+
+$(document).ready(function () {
+    $('.slider_area').bxSlider();
+    $('#main_menu').meanmenu({
+          meanMenuContainer:'#mobile_menu',
+          meanScreenWidth:991,
+
+
+    });
+});
